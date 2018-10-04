969232f1edf2a2af644885a780f15c4b83c3741c
Sean Hung <35648173+seanhung07@users.noreply.github.com>
2018-10-04 08:13:12 +0800

    Update about.php

--- about.php
+++ about.php
@@ -4,8 +4,6 @@
   <div class="info" style="font-size:200%"><strong>Phone: </strong>0987654321</div>
   <div class="info" style="font-size:200%"><strong>Open: </strong>9:00am</div>
   <div class="info" style="font-size:200%"><strong>Close: </strong>12:00pm</div>
-  <div class="info" style="font-size:200%"><strong>History: </strong>The Pizza story begins in September 2018. When Daniel, Sean, and Jessie decided to learn to develop a website, they found their interest in pizza.  The founder of pizza said "The people were enchanted with the product, watching us tossing the dough over our heads. They were really enjoying themselves…." Daniel, Sean, and Jessie created an innovative, high quality, engaging, informal eating experience in a friendly neighborhood restaurant. Pizza became a popular place with the teenage and college crowd. 
-Pizza is always an entrepreneurial, innovative, fast-thinking place. It is the kind of restaurant where everyone shared the learning. As the founder said "To me, the biggest strength of Pizza was that we had an awful lot of people who came up with great ideas."</div>
 <div class="info" style="font-size:200%"><strong>Core Value: </strong> The Right People, Enterprise, Energy, Performance</div>
 
 </div>
